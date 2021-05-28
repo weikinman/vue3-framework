2a5a768aa739cd2085f2c71f1586d0c41aaa998a
scott <scott@boardware.com>
2021-05-28 14:28:34 +0800

    f

--- packages/vue3-framework/index.ts
+++ packages/vue3-framework/index.ts
@@ -8,7 +8,7 @@ import type { InstallOptions } from '@vue3-framework/utils/config'
 
 const version = version_ // version_ to fix tsc issue
 
-console.log(version+'22fff23fffff3');
+console.log(version+'22fff2ff3fffff3');
 const defaultInstallOpt: InstallOptions =  {
   size: '' as ComponentSize,
   zIndex: 2000,
