6465e3e224c6ab6f98b084578efaf41c8abc9021
scott <scott@boardware.com>
2021-05-28 10:51:23 +0800

    feat: add Fiels

---
+++ packages/http/__tests__/http.test.js
@@ -0,0 +1,7 @@
+'use strict'
+
+const http = require('..')
+
+describe('@vue3-framework/http', () => {
+  it('needs tests')
+})
---
+++ packages/vue3-framework/index.ts
@@ -0,0 +1,46 @@
+import type { App } from 'vue'
+
+// if you encountered problems alike "Can't resolve './version'"
+// please run `yarn bootstrap` first
+import { version as version_ } from './version'
+import { setConfig } from '@vue3-framework/utils/config'
+import type { InstallOptions } from '@vue3-framework/utils/config'
+
+const version = version_ // version_ to fix tsc issue
+
+
+const defaultInstallOpt: InstallOptions =  {
+  size: '' as ComponentSize,
+  zIndex: 2000,
+}
+
+const components = [
+]
+
+const plugins = [
+]
+
+const install = (app: App, opt: InstallOptions): void => {
+  const option = Object.assign(defaultInstallOpt, opt)
+
+  app.config.globalProperties.$ELEMENT = option
+  setConfig(option)
+
+  components.forEach(component => {
+    app.component(component.name, component)
+  })
+
+  plugins.forEach(plugin => {
+    app.use(plugin as any)
+  })
+}
+
+export {
+  version,
+  install,
+}
+
+export default {
+  version,
+  install,
+}
